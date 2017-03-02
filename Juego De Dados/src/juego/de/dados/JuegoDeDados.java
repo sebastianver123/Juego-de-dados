6e7ff8b4c7da03da172c9d386d37e50421cdcbf9
labing506 <labing506@labing50614>
2017-03-01 22:11:03 -0500

    Dado

---
+++ Juego De Dados/src/juego/de/dados/JuegoDeDados.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego.de.dados;
+
+/**
+ *
+ * @author labing506
+ */
+public class JuegoDeDados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Dado1 unDado=new Dado1();
+        Dado1 otroDado=new Dado1();
+        
+        
+        
+        
+        Grafico miGrafico=new Grafico();
+        miGrafico.setVisible(true);
+    }
+    
+}
